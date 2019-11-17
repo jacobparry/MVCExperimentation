c5963aa2dee05fc64596c51e08856679337fcce5
Jacob Parry <griever815@gmail.com>
2019-11-17 16:12:33 -0700

    Initial upload

---
+++ MVCExperimentation/Data/MVCExperimentationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCExperimentation.Models
+{
+    public class MVCExperimentationContext : DbContext
+    {
+        public MVCExperimentationContext (DbContextOptions<MVCExperimentationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCExperimentation.Models.Movie> Movie { get; set; }
+    }
+}
